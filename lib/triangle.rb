941e68c5b536beadc9221963aaee3e4b691b3dd0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-03-24 10:49:25 -0400

    commit tree ce163dd9612953cc356c67df65a6f356bce7484a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,7 @@ class Triangle
     @length_1 = length_1
     @length_2 = length_2
     @length_3 = length_3
+
   end
 
   def kind
@@ -34,6 +35,6 @@ class Triangle
     end
   end
 
-#   new = Triangle.new(50, 20, 10)
+#   new = Triangle.new(10, 10, 20)
 # binding.pry
 end
